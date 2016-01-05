6e405f90d1ffd791fcaedf3364a607a1495c43dd
Koen Vervoordeldonk <koen@debian-notebook.fritz.box>
2016-01-05 13:00:48 +0100

    Added some grunt-dependencies

---
+++ target-grunt/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+ * @ngdoc function
+ * @name t2C3AngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the t2C3AngularApp
+ */
+angular.module('t2C3AngularApp')
+    .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+    ];
+});
+//# sourceMappingURL=about.js.map
