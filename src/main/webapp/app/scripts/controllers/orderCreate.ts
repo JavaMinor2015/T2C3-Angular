70b48229e84b8f2c379f6ce32b25b25e5f7b8907
Wuis <m.wuis@live.nl>
2016-01-14 13:28:55 +0100

    order create prefilled

--- src/main/webapp/app/scripts/controllers/orderCreate.ts
+++ src/main/webapp/app/scripts/controllers/orderCreate.ts
@@ -15,22 +15,22 @@
 angular.module('t2C3AngularApp')
   .controller('OrderCtrl', ['$scope', 'orderService', '$location', 'orderResource', 'UserService'
     , function ($scope, orderService, $location, orderResource, userService) {
-        if(userService.isLoggedIn()){
-          let customer : t2C3AngularApp.Customer;
-          customer = userService.getCustomer();
-          let address : t2C3AngularApp.Address;
-          address = customer.address;
-          $scope.firstName = customer.firstName;
-          $scope.lastName = customer.lastName;
-          $scope.emailAddress = customer.email;
-          if(address) {
-            $scope.straatnaam = address.street;
-            $scope.straatnummer = address.streetNumber;
-            $scope.city = address.city;
-            $scope.zipcode = address.zipcode;
-          }
-
+      if (userService.isLoggedIn()) {
+        let customer : t2C3AngularApp.Customer;
+        customer = userService.getCustomer();
+        let address : t2C3AngularApp.Address;
+        address = customer.address;
+        $scope.firstName = customer.firstName;
+        $scope.lastName = customer.lastName;
+        $scope.emailAddress = customer.email;
+        if (address) {
+          $scope.straatnaam = address.street;
+          $scope.straatnummer = address.streetNumber;
+          $scope.city = address.city;
+          $scope.zipcode = address.zipcode;
         }
+
+      }
       $scope.placeOrder = function () {
         console.log('clicked order');
 
