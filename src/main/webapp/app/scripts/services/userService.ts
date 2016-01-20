346248f47c8ec287bdc957388af96fbb7f21e9ba
Wuis <m.wuis@live.nl>
2016-01-20 14:05:13 +0100

    better streetnumber assignment

--- src/main/webapp/app/scripts/services/userService.ts
+++ src/main/webapp/app/scripts/services/userService.ts
@@ -46,7 +46,7 @@ module t2C3AngularApp {
         if (data.address.street) {
           address.setStreet(data.address.street);
         }
-        if (data.address.streetnumber) {
+        if (data.address.streetNumber) {
           address.setStreetNumber(data.address.streetNumber);
         }
         if (data.address.zipcode) {
